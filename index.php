0dfc310a631df26bd1f52d7bd85e69dfed396713
Emmanuel Palomeque Alcázar <epalomeque@gmail.com>
2025-06-04 19:07:58 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Arte AngieBu Theme
+ *
+ * @package arte-angiebu
+ * @since 1.0
+ */
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Arte AngieBu</title>
+</head>
+<body>
+  Hello wordpress theme!
+  <h1>Welcome to Arte AngieBu Theme</h1>
+</body>
+</html>
